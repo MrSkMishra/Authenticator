331a1b2d24404cb6f65321c5d9dff3aca61bc814
Sonu Kumar Mishra <mishrasonu9211@gmail.com>
2023-01-03 13:09:49 +0530

    everything is completed

--- login/registration/views.py
+++ login/registration/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url='login')
 def HomePage(request):
-
     return render(request,"home.html")
 
 
